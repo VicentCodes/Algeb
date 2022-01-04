401b94361c7c887938f0544c4b5123ec7ffe1145
vicen <vicnete.7nevarez@gmail.com>
2022-01-04 11:09:50 -0600

    Correccion de bugs

---
+++ app/src/main/java/com/vicentcode/algeb/Splash.kt
@@ -0,0 +1,25 @@
+package com.vicentcode.algeb
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.example.algeb.R
+
+class Splash : AppCompatActivity() {
+    lateinit var handler: Handler
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        handler = Handler()
+        handler.postDelayed({
+
+            // Delay and Start Activity
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        } , 3000)
+
+    }
+}
